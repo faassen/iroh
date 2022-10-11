5c237b2d9db21fa87bd6515f63189dbdff812b7e
Martijn Faassen <faassen@startifact.com>
2022-10-11 16:46:24 +0200

    Get rid of async_trait magic in the hopes I can specify get_stream better

--- iroh-api/src/api.rs
+++ iroh-api/src/api.rs
@@ -6,9 +6,10 @@ use crate::config::{Config, CONFIG_FILE_NAME, ENV_PREFIX};
 use crate::p2p::MockP2p;
 use crate::p2p::{ClientP2p, P2p};
 use anyhow::Result;
-use async_trait::async_trait;
 use cid::Cid;
+use futures::future::{BoxFuture, LocalBoxFuture};
 use futures::stream::LocalBoxStream;
+use futures::FutureExt;
 use futures::StreamExt;
 use iroh_resolver::resolver::Path as IpfsPath;
 use iroh_resolver::{resolver, unixfs_builder};
@@ -28,16 +29,24 @@ pub enum OutType<T: resolver::ContentLoader> {
 }
 
 #[cfg_attr(feature= "testing", automock(type P = MockP2p;))]
-#[async_trait(?Send)]
 pub trait Api {
     type P: P2p;
 
     fn p2p(&self) -> Result<Self::P>;
 
-    async fn get<'a>(&self, ipfs_path: &IpfsPath, output: Option<&'a Path>) -> Result<()>;
-    async fn add(&self, path: &Path, recursive: bool, no_wrap: bool) -> Result<Cid>;
-    async fn check(&self) -> StatusTable;
-    async fn watch<'a>(&self) -> LocalBoxStream<'a, StatusTable>;
+    fn get<'a>(
+        &'a self,
+        ipfs_path: &'a IpfsPath,
+        output: Option<&'a Path>,
+    ) -> BoxFuture<'_, Result<()>>;
+    fn add<'a>(
+        &'a self,
+        path: &'a Path,
+        recursive: bool,
+        no_wrap: bool,
+    ) -> LocalBoxFuture<'_, Result<Cid>>;
+    fn check(&self) -> BoxFuture<'_, StatusTable>;
+    fn watch(&self) -> LocalBoxFuture<'static, LocalBoxStream<'static, StatusTable>>;
 }
 
 impl Iroh {
@@ -73,7 +82,6 @@ impl Iroh {
     }
 }
 
-#[async_trait(?Send)]
 impl Api for Iroh {
     type P = ClientP2p;
 
@@ -82,45 +90,63 @@ impl Api for Iroh {
         Ok(ClientP2p::new(p2p_client.clone()))
     }
 
-    async fn get<'b>(&self, ipfs_path: &IpfsPath, output: Option<&'b Path>) -> Result<()> {
-        let blocks = self.get_stream(ipfs_path, output);
-        tokio::pin!(blocks);
-        while let Some(block) = blocks.next().await {
-            let (path, out) = block?;
-            match out {
-                OutType::Dir => {
-                    tokio::fs::create_dir_all(path).await?;
-                }
-                OutType::Reader(mut reader) => {
-                    if let Some(parent) = path.parent() {
-                        tokio::fs::create_dir_all(parent).await?;
+    fn get<'a>(
+        &'a self,
+        ipfs_path: &'a IpfsPath,
+        output: Option<&'a Path>,
+    ) -> BoxFuture<'_, Result<()>> {
+        async move {
+            let blocks = self.get_stream(ipfs_path, output);
+            tokio::pin!(blocks);
+            while let Some(block) = blocks.next().await {
+                let (path, out) = block?;
+                match out {
+                    OutType::Dir => {
+                        tokio::fs::create_dir_all(path).await?;
                     }
-                    let mut f = tokio::fs::File::create(path).await?;
-                    tokio::io::copy(&mut reader, &mut f).await?;
+                    OutType::Reader(mut reader) => {
+                        if let Some(parent) = path.parent() {
+                            tokio::fs::create_dir_all(parent).await?;
+                        }
+                        let mut f = tokio::fs::File::create(path).await?;
+                        tokio::io::copy(&mut reader, &mut f).await?;
+                    }
                 }
             }
+            Ok(())
         }
-        Ok(())
+        .boxed()
     }
 
-    async fn add(&self, path: &Path, recursive: bool, no_wrap: bool) -> Result<Cid> {
-        let providing_client = iroh_resolver::unixfs_builder::StoreAndProvideClient {
-            client: Box::new(self.get_client()),
-        };
-        if path.is_dir() {
-            unixfs_builder::add_dir(Some(&providing_client), path, !no_wrap, recursive).await
-        } else if path.is_file() {
-            unixfs_builder::add_file(Some(&providing_client), path, !no_wrap).await
-        } else {
-            anyhow::bail!("can only add files or directories");
+    fn add<'a>(
+        &'a self,
+        path: &'a Path,
+        recursive: bool,
+        no_wrap: bool,
+    ) -> LocalBoxFuture<'_, Result<Cid>> {
+        async move {
+            let providing_client = iroh_resolver::unixfs_builder::StoreAndProvideClient {
+                client: Box::new(self.get_client()),
+            };
+            if path.is_dir() {
+                unixfs_builder::add_dir(Some(&providing_client), path, !no_wrap, recursive).await
+            } else if path.is_file() {
+                unixfs_builder::add_file(Some(&providing_client), path, !no_wrap).await
+            } else {
+                anyhow::bail!("can only add files or directories");
+            }
         }
+        .boxed_local()
     }
 
-    async fn check(&self) -> StatusTable {
-        self.client.check().await
+    fn check(&self) -> BoxFuture<'_, StatusTable> {
+        async { self.client.check().await }.boxed()
     }
 
-    async fn watch<'b>(&self) -> LocalBoxStream<'b, iroh_rpc_client::StatusTable> {
-        self.client.clone().watch().await.boxed()
+    fn watch(
+        &self,
+    ) -> LocalBoxFuture<'static, LocalBoxStream<'static, iroh_rpc_client::StatusTable>> {
+        let client = self.client.clone();
+        async { client.watch().await.boxed_local() }.boxed_local()
     }
 }
